85e88c3802236a5943f3856a36e5cc6ddd1711a0
BrhmDnsK263 <Denysiuk.ISI@gmail.com>
2017-12-11 19:45:31 -0300

    agregado class y placeholder a un campo en un form para pruebas

--- perros/app_campaing/forms.py
+++ perros/app_campaing/forms.py
@@ -24,3 +24,10 @@ class CreateCampaing(forms.ModelForm):
 		model = Campaing
 		fields =  ("fecha","lugar", "tipo", "monto_valor_operacion","monto_inter_grupo_total")
 		labels = {"fecha":"Fecha","lugar":"Lugar", "tipo":"Tipo","monto_valor_operacion":"Valor Intervención", "monto_inter_grupo_total":"Valor Intervenciones Cubiertas"}
+
+	def __init__(self, *args, **kwargs):
+
+		super(CreateCampaing, self).__init__(*args, **kwargs)
+		#self.fields['nombre'].widget.attrs.update({'class' : 'mi_clase_nombre','placeholder' : 'Nombre'})
+		self.fields['lugar'].widget.attrs.update({'class' : 'mi_clase_nombre','placeholder' : 'Lugar'})
+		
