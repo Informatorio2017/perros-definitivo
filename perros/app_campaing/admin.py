726c70b9d234a3bff26d40f5b8832d7be1be1c00
nelson-nunez <nelsonjnunez@gmail.com>
2017-12-13 17:02:38 -0300

    agregado ver campañas

--- perros/app_campaing/admin.py
+++ perros/app_campaing/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Lugar
+from .models import Campaing
 
 
 # Register your models here.
 
 
 admin.site.register(Lugar)
+admin.site.register(Campaing)
--- perros/app_campaing/urls.py
+++ perros/app_campaing/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 
     url(r'^create_campaing/', views.create_campaing, name="create_campaing"),
     url(r'^creado/', views.creado, name="creado"),
-   
+    url(r'^ver_campanas/', views.ver_campanas, name="ver_campanas"),
 
 ]
