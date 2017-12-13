70ec3f9f41db75a1cbd63698bb64b77bbea1ca77
matiasfm89 <mancuellomatias@gmail.com>
2017-12-13 19:26:57 -0300

    agrego home_admin

--- perros/app_campaing/urls.py
+++ perros/app_campaing/urls.py
@@ -5,15 +5,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
-  
-
     url(r'^create_campaing/', views.create_campaing, name="create_campaing"),
     url(r'^creado/', views.creado, name="creado"),
-
-
-
-	url(r'^home/', views.home, name="home"),    
-   
-
+	url(r'^home_admin/', views.home_admin, name="home_admin"),    
 ]
--- perros/app_campaing/views.py
+++ perros/app_campaing/views.py
@@ -1,41 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import CreateCampaing
-
-
-
+from .models import Campaing
 
 def creado(request):
 	return render(request, 'creado.html', {})	
 
-
 def create_campaing(request):
-
-	
-
     if request.method == 'POST':
-
         form = CreateCampaing(request.POST)
-
-        if form.is_valid():
-        	
-
+        if form.is_valid():        
         	form.save()
         	return redirect('/campaing/creado/')
-
-
-        else:
-        	
+        else:        
         	return redirect('/Aca_si_no_valida_los_datos')
-
     else:
     	contexto = {"form":CreateCampaing}
-    	return render(request, "create_campaing.html", contexto)
-    	
+    	return render(request, "create_campaing.html", contexto)    	
 
-    
-        
-
-def home(request):
-
-    contexto = {}
-    return render(request, "home.html", contexto)
+def home_admin(request):
+    campaing = Campaing.objects.filter(habilitada=True)
+    contexto = {'campaing':campaing}
+    return render(request, "home_admin.html", contexto)
