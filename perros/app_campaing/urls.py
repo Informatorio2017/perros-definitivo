fc42499e47f13567033bcac66a27f358a9c8c81c
BrhmDnsK263 <Denysiuk.ISI@gmail.com>
2017-12-15 17:35:27 -0300

    arreglos 2

--- perros/app_campaing/urls.py
+++ perros/app_campaing/urls.py
@@ -9,40 +9,24 @@ urlpatterns = [
     url(r'^creado/', views.creado, name="creado"),
     url(r'^ver_colaboradores/', views.ver_colaboradores, name="ver_colaboradores"),
 
-
-
     #home campaing
 	url(r'^home/', views.home, name="home"),
+	url(r'^home_admin/', views.home_admin, name="home_admin"),    
 
 	#
 	url(r'^buscar_paciente/', views.buscar_paciente, name="buscar_paciente"),
 
-
-	
-	
 	#aca esta para buscar preinscriptos??? y despues ir a la ventana formulario inscripcion??
 	url(r'^inscribir_paciente_pre/', views.inscribir_paciente_pre, name="inscribir_paciente_pre"),
 
 	url(r'^formulario_inscripcion/', views.formulario_inscripcion, name="formulario_inscripcion"),
-
-
 	
 	url(r'^pre_inscribirse/', views.pre_inscribirse, name="pre_inscribirse"),
 	
-
-	
 	url(r'^alta_paciente/([0-9]+)$', views.alta_paciente, name="alta_paciente"),
-
-
-
-	url(r'^formulario_inscripcion/', views.formulario_inscripcion, name="formulario_inscripcion"),
-
-
-
 
     url(r'^ver_campanas/', views.ver_campanas, name="ver_campanas"),
 
 
-	url(r'^home_admin/', views.home_admin, name="home_admin"),    
 
 ]
