a22a803812b90c182e8ea499bc91e332cc382d05
BrhmDnsK263 <Denysiuk.ISI@gmail.com>
2018-01-04 13:39:36 -0300

    static file root

--- perros/perros/settings.py
+++ perros/perros/settings.py
@@ -17,6 +17,7 @@ var = "hola mundo"
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'imagenes')
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
